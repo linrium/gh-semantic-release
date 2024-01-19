1886fbe62ffead1816f894ca5cd16fd2dd62bcda
linrium <linrium@gmail.com>
2024-01-19 16:34:05 +0900

    feat: init commit

---
+++ release.config.js
@@ -0,0 +1,25 @@
+const config = {
+  branches: ["main"],
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      }
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      }
+    ],
+    "@semantic-release/github"
+  ]
+}
+
+module.exports = config
